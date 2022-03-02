82cfb18fa24e262d942e0d1d4b515329eae42d55
rashidgafurov <nsd7c4@gmail.com>
2022-03-02 16:58:28 -0500

    Howework 2 exercise 1

--- src/com/RashidGafurov_INC_LOL/Main.java
+++ src/com/RashidGafurov_INC_LOL/Main.java
@@ -3,6 +3,14 @@ package com.RashidGafurov_INC_LOL;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        byte byteVar1 = 1;
+        short shortVar1 = 2;
+        int intVar1 = 3;
+        long longVar1 = 4L;
+        float floatVar1 = 5.5f;
+        double doubleVar1 = 6.6;
+        char charVar1 = 'R';
+        boolean booleanVar1 = true;
+
     }
 }
